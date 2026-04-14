5d66760de40889b5dfe518636b6168009024fbbe
fernando6767 <ferjorodriguez300@gmail.com>
2026-04-14 13:33:07 -0700

    Initial Commit

---
+++ Wu-Tang App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wu-Tang App
+//
+//  Created by RODRIGUEZ RIVERA, FERNANDO on 4/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
